de570825752f8f5a713062894a2ca3fbc296c055
Minreng Wu <wumrwds@gmail.com>
2020-12-07 21:01:11 -0600

    feat: complete the docker compose files for the flink 1.10.2 standalone cluster

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+TAG=1.10.2
+
+build() {
+    NAME=$1
+    IMAGE=bde2020/flink-$NAME:$TAG
+    cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
+    echo '--------------------------' building $IMAGE in $(pwd)
+    docker build -t $IMAGE .
+    cd -
+}
+
+build base
+build master
+build worker
